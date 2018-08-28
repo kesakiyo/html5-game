4e6eb79485f8c56a44ec83f943dcadccc3c36891
Minho Cheon <mino@Minhoui-MacBook-Pro.local>
2018-08-29 03:41:38 +0900

    움직이는 공이 모서리에 닿았을 때 방향과 색이 변하는 기능 추가

--- breakout-pure-js/breakout.js
+++ breakout-pure-js/breakout.js
@@ -1,23 +1,45 @@
+const getRandomColor = function() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 const draw = (function () {
 	var canvas = document.getElementById('myCanvas');
 	var ctx = canvas.getContext('2d');
 
+	var ballRadius = 10;
+	var ballColor = getRandomColor();
 	var x = canvas.width / 2;
 	var y = canvas.height - 30;
 	var dx = 2;
 	var dy = -2;
 
-	const drawBall = function(ctx, x, y) {
+	const drawBall = function(ctx, color, radius, x, y) {
 		ctx.beginPath();
-		ctx.arc(x, y, 10, 0, Math.PI * 2);
-		ctx.fillStyle = '#0095DD';
+		ctx.arc(x, y, radius, 0, Math.PI * 2);
+		ctx.fillStyle = color;
 		ctx.fill();
 		ctx.closePath();
 	}
 
 	return function() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		drawBall(ctx, x, y);
+		drawBall(ctx, ballColor, ballRadius, x, y);
+
+		if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+			dy = -dy;
+			ballColor = getRandomColor();			
+		}
+		if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+			dx = -dx;
+			changedDirection = true;
+			ballColor = getRandomColor();
+		}
+
 		x += dx;
 		y += dy;
 	}
