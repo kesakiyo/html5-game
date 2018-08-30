9b023ed4f36db5d93ec7f6c569d00fa2e9c28493
Minho Cheon <mino@Minhoui-MacBook-Pro.local>
2018-08-31 02:32:38 +0900

    사용자 입력에 반응하는 paddle 추가

--- breakout-pure-js/breakout.js
+++ breakout-pure-js/breakout.js
@@ -11,12 +11,34 @@ const draw = (function () {
 	var canvas = document.getElementById('myCanvas');
 	var ctx = canvas.getContext('2d');
 
+	var paddleHeight = 10;
+	var paddleWidth = 75;
+	var paddleX = (canvas.width - paddleWidth) / 2;
+	var leftPressed = false;
+	var rightPressed = false;
+
 	var ballRadius = 10;
 	var ballColor = getRandomColor();
+
 	var x = canvas.width / 2;
 	var y = canvas.height - 30;
 	var dx = 2;
 	var dy = -2;
+
+	document.addEventListener('keydown', function(e) {
+		if (e.keyCode === 39) {
+			rightPressed = true;
+		} else if (e.keyCode === 37) {
+			leftPressed = true;
+		}
+	}, false);
+	document.addEventListener('keyup', function(e) {
+		if (e.keyCode === 39) {
+			rightPressed = false;
+		} else if (e.keyCode === 37) {
+			leftPressed = false;
+		}
+	}, false);
 
 	const drawBall = function(ctx, color, radius, x, y) {
 		ctx.beginPath();
@@ -26,9 +48,18 @@ const draw = (function () {
 		ctx.closePath();
 	}
 
+	const drawPaddle = function(ctx, x, y) {
+		ctx.beginPath();
+		ctx.rect(x, y, paddleWidth, paddleHeight);
+		ctx.fillStyle = '#0095DD';
+		ctx.fill();
+		ctx.closePath();
+	}
+
 	return function() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		drawBall(ctx, ballColor, ballRadius, x, y);
+		drawPaddle(ctx, paddleX, canvas.height - paddleHeight);
 
 		if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
 			dy = -dy;
@@ -42,6 +73,12 @@ const draw = (function () {
 
 		x += dx;
 		y += dy;
+
+		if (rightPressed && paddleX < canvas.width - paddleWidth) {
+			paddleX += 7;
+		} else if (leftPressed && paddleX > 0) {
+			paddleX -= 7;
+		}
 	}
 })();
 
